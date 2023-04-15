2004970e2a20c05468fc712109535fa0b5557266
GabrielGML <ggbielgomes3@gmail.com>
2023-04-14 23:15:22 -0300

    Aula 2: obstacle

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,4 +1,6 @@
 import pygame
+from pygame.sprite import Sprite
+
 from dino_runner.utils.constants import RUNNING, JUMPING, DUCKING
 
 X_POS = 80
@@ -29,9 +31,11 @@ class Dinosaur:
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
+            self.dino_duck = False
         elif user_input[pygame.K_DOWN] and not self.dino_jump:
             self.dino_duck = True
             self.dino_run = False
+            self.dino_jump = False
         elif not self.dino_jump:
             self.dino_jump = False
             self.dino_duck = False
@@ -39,13 +43,6 @@ class Dinosaur:
 
         if self.step_index >= 10:
             self.step_index = 0
-
-        if user_input[pygame.K_DOWN] and not self.dino_jump:
-            self.dino_duck = True
-            self.dino_run = False
-        elif not self.dino_jump:
-            self.dino_duck = False
-            self.dino_run = True
 
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
