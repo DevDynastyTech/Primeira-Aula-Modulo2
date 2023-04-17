dfe4fdf2fc8281750fd7695458c3f7458b55ac2f
GabrielGML <ggbielgomes3@gmail.com>
2023-04-17 19:53:58 -0300

    atividade 2: LargeCactus e Bird

---
+++ dino_runner/components/obstacles/bird.py
@@ -0,0 +1,22 @@
+import random
+
+from dino_runner.components.obstacles.obstacle import Obstacle
+
+
+class Bird(Obstacle):
+    def __init__(self, image):
+        self.type = 0
+        super().__init__(image, self.type)
+        self.rect.y = random.randint(180, 320)
+
+    def update(self, game_speed, obstacles):
+        self.rect.x -= game_speed
+
+        if self.type >= 10:
+            self.type = 0
+
+        super().update(game_speed, obstacles)
+
+    def draw(self, screen):
+        screen.blit(self.image[self.type//5], (self.rect.x, self.rect.y))
+        self.type += 1
--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,7 +1,9 @@
+import random
 import pygame
 
-from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.bird import Bird
+from dino_runner.components.obstacles.cactus import Cactus, LargeCactus
+from dino_runner.utils.constants import SMALL_CACTUS, LARGE_CACTUS, BIRD
 
 
 class ObstacleManager:
@@ -10,7 +12,14 @@ class ObstacleManager:
 
     def update(self, game):
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
+            obstacle_type = random.randint(0, 2)
+            if obstacle_type == 0:
+                self.obstacles.append(Cactus(SMALL_CACTUS))
+            elif obstacle_type == 1:
+                self.obstacles.append(LargeCactus(LARGE_CACTUS))
+            
+            elif obstacle_type == 2:
+                self.obstacles.append(Bird(BIRD))
 
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
